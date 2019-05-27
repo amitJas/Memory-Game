38d7fbb76e73846bd378f130fc0ba37b5a5c0b6a
amitJas <katanamit7@gmail.com>
2019-05-27 16:22:16 +0300

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,15 +22,16 @@ export class AppComponent {
   
   constructor(private zone:NgZone ){}
   
-  logic(event,card){
+  gameLogic(card){
 
   this.countEvent++;
 
-  if(this.countEvent == 1 ){ // first carde chosen
+  if(this.countEvent == 1 && card.bingo != true  ){ // first carde chosen
+    console.log('in')
     this.first = card;
     this.first.side = true
   }
-    if(this.countEvent == 2){ //second card chosen
+    if(this.countEvent == 2 && card.bingo != true ){ //second card chosen
       this.second = card;
       this.second.side = true
 
